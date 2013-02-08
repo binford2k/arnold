e893690707fb45728cc5d60ac89f918633dbfe71
Ben Ford <ben.ford@puppetlabs.com>
2013-02-07 18:16:13 -0800

    Yay for typos.

--- arnold/lib/puppet/parser/functions/arnold_params.rb
+++ arnold/lib/puppet/parser/functions/arnold_params.rb
@@ -1,5 +1,5 @@
 Puppet::Parser::Functions::newfunction(:arnold_params, :doc => "Populate current scope with parameters from Arnold") do
-  params = function_hiera(['parameters', undef, ['arnold/nodename/%{fqdn}','arnold/macaddr/%{macaddress}']])
+  params = function_hiera(['parameters', nil, ['arnold/nodename/%{fqdn}','arnold/macaddr/%{macaddress}']])
   params.each do |name, value|
     setvar(name, value)
   end
