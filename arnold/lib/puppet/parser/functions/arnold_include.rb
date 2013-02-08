3d03fbcc62baff799bd19a6a3a1ddc858edf1e35
Ben Ford <ben.ford@puppetlabs.com>
2013-02-08 10:36:48 -0800

    Fixed default values

--- arnold/lib/puppet/parser/functions/arnold_include.rb
+++ arnold/lib/puppet/parser/functions/arnold_include.rb
@@ -1,3 +1,9 @@
 Puppet::Parser::Functions::newfunction(:arnold_include, :doc => "Include classes from Arnold") do
-  function_hiera_include(['classes', nil, ['arnold/nodename/%{fqdn}','arnold/macaddr/%{macaddress}']])
+  classes = function_hiera_array(['classes', ['*****'], ['arnold/nodename/%{fqdn}','arnold/macaddr/%{macaddress}']])
+
+  # The version of hiera_array() shipping in Puppet 2.7 won't let you default to nothing
+  return if classes = ['*****']
+  classes.each do |name|
+    function_include(name)
+  end
 end
